d3f53f3e929279e9892b6f4059857da2a99599e7
Sherysh <katesherysh@gmail.com>
2021-09-09 19:35:12 +0300

    sound

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
@@ -10,12 +11,15 @@ public class Menu : MonoBehaviour
     [SerializeField] private Button _startButton;
     [SerializeField] private Button _optionsButton;
     [SerializeField] private Button _exitButton;
+    [SerializeField] private Slider _masterVolume;
+    public AudioMixer masterMixer;
 
     void Start()
     {
         _startButton.onClick.AddListener(StartGame);
         _optionsButton.onClick.AddListener(Options);
         _exitButton.onClick.AddListener(Exit);
+        _masterVolume.onValueChanged.AddListener((float value) => { SetSound(value * 50); });
     }
 
     void StartGame()
@@ -25,12 +29,17 @@ public class Menu : MonoBehaviour
 
     void Options()
     {
-
+        
     }
 
     void Exit()
     {
         Application.Quit();
+    }
+
+    void SetSound(float soundLevel)
+    {
+        masterMixer.SetFloat("MasterVol", soundLevel);
     }
 
 }
